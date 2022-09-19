9b6a3a16c7065ad63941d3d4eac8803193ceb7d9
Rafael <rafael@Rafaels-MacBook-Air.local>
2022-09-18 21:31:28 -0300

    Added DeletedPosts

--- resources/js/Components/Post.vue
+++ resources/js/Components/Post.vue
@@ -21,17 +21,16 @@
 
     <template>
         <div class="p-6 flex space-x-2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-            </svg>
+            <img class="h-10 w-10" src="../../css/alien-monster_1f47e.png" alt="alien">
             <div class="flex-1">
                 <div class="flex justify-between items-center">
                     <div>
                         <span class="text-gray-800">{{ post.user.name }}</span>
-                        <small class="ml-2 text-sm text-gray-600">{{ dayjs(post.created_at).fromNow() }}</small>
-                        <small v-if="post.created_at !== post.updated_at" class="text-sm text-gray-600"> &middot; edited</small>
+                        <small class="ml-2 text-sm text-gray-600">{{ dayjs(post.created_at).fromNow() }} &middot; created</small>
+                        <small v-if="post.created_at !== post.updated_at && !post.deleted_at" class="text-sm text-gray-600"> &middot; edited</small>
+                        <small v-if="post.deleted_at" class="ml-2 text-sm text-gray-600">{{ dayjs(post.deleted_at).fromNow() }} &middot; deleted</small>
                     </div>
-                    <Dropdown v-if="post.user.id === $page.props.auth.user.id">
+                    <Dropdown v-if="post.user.id === $page.props.auth.user.id && !post.deleted_at">
                     <template #trigger>
                         <button>
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
---
+++ resources/js/Pages/Posts/DeletedPosts.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import Post from '@/Components/Post.vue';
+    import InputError from '@/Components/InputError.vue';
+    import PrimaryButton from '@/Components/PrimaryButton.vue';
+    import { useForm, Head } from '@inertiajs/inertia-vue3';
+
+    defineProps(['deleted_posts']);
+    </script>
+
+    <template>
+        <Head title="Deleted Posts" />
+
+        <AuthenticatedLayout>
+            <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                <h1>Deleted Posts</h1>
+                <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                    <Post
+                        v-for="post in deleted_posts"
+                        :key="post.id"
+                        :post="post"
+                    />
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    </template>
